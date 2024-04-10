76cb622a340f7e366297f4bb8597cd2352e073cd
Prashant047 <prash.kumar047@gmail.com>
2024-04-10 20:23:16 +0100

    initial commit

---
+++ app/fakeData.ts
@@ -0,0 +1,86 @@
+const fakeData1 = [
+  {
+    year: '2012',
+    val: 3
+  },
+  {
+    year: '2013',
+    val: 1
+  },
+  {
+    year: '2014',
+    val: -1
+  },
+  {
+    year: '2015',
+    val: 0
+  },
+  {
+    year: '2016',
+    val: 0
+  },
+  {
+    year: '2017',
+    val: -1
+  },
+  {
+    year: '2018',
+    val: -1
+  },
+  {
+    year: '2019',
+    val: -1
+  },
+  {
+    year: '2020',
+    val: -2
+  },
+  {
+    year: '2021',
+    val: -2
+  },
+];
+
+const fakeData2 = [
+  {
+    year: '2012',
+    val: 1
+  },
+  {
+    year: '2013',
+    val: -1
+  },
+  {
+    year: '2014',
+    val: -3
+  },
+  {
+    year: '2015',
+    val: -2
+  },
+  {
+    year: '2016',
+    val: -3
+  },
+  {
+    year: '2017',
+    val: -3
+  },
+  {
+    year: '2018',
+    val: -4
+  },
+  {
+    year: '2019',
+    val: 3
+  },
+  {
+    year: '2020',
+    val: -4
+  },
+  {
+    year: '2021',
+    val: -4
+  },
+];
+export {fakeData1, fakeData2};
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,176 @@
-import Image from "next/image";
+"use client";
+import { useMeasure } from '@uidotdev/usehooks';
+import * as d3 from 'd3';
+import {fakeData1, fakeData2} from './fakeData';
+
 
 export default function Home() {
+  const [ref, { height, width }] = useMeasure();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main
+      className='mx-auto max-w-3xl h-[400px] my-20'
+      ref={ref}
+    >
+      {height&&width&&<Graph height={height} width={width}/>}
+      <p className='text-center text-sm'>
+        Original Design - <a href="https://www.behance.net/gallery/54656777/Media-Economy-Report-Vol10" className='hover:underline' target='blank'>https://www.behance.net/gallery/54656777/Media-Economy-Report-Vol10</a>
+      </p>
     </main>
   );
+}
+
+function Graph({height, width}: {height: number, width:number}) {
+  const paddingX = 50;
+  const paddingY = 80;
+
+  const xScale = d3.scaleBand(fakeData1.map(data => data.year), [paddingX, width-paddingX])
+    .paddingOuter(0)
+    .paddingInner(0.7)
+  const yScale = d3.scaleLinear([5, -5], [paddingY, height-paddingY])
+  const yScaleTicks = yScale.ticks(5);
+    
+
+  const curveGenerator = d3.line()
+  // @ts-ignore
+    .x(d => xScale(d.year) + xScale.bandwidth()/2)
+  // @ts-ignore
+    .y(d => yScale(d.val))
+    .curve(d3.curveMonotoneX)
+
+  // @ts-ignore
+  const curvePath1 = curveGenerator(fakeData1);
+  // @ts-ignore
+  const curvePath2 = curveGenerator(fakeData2);
+
+  return (
+    <div>
+      <svg height={height} width={width}>
+        <pattern id="diagonalHatch" patternUnits="userSpaceOnUse" width="4" height="4">
+          <path d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2" style={{stroke: 'currentcolor', strokeWidth: 1}} />
+        </pattern>
+        {fakeData1.map((data, index) => {
+          return (
+            <g
+              key={index}
+            >
+              <text
+                className='text-md font-mono font-bold'
+                // @ts-ignore
+                transform={`translate(${xScale(data.year) + xScale.bandwidth()/2 + 5}, ${yScale(5)-20}) rotate(-90)`}
+              >
+                {data.year}
+              </text>
+              <rect
+                x={xScale(data.year)}
+                y={yScale(5)}
+                height={yScale(-5) -yScale(5)}
+                width={xScale.bandwidth()}
+                fill='url(#diagonalHatch)'
+                fillOpacity={0.2}
+              />
+            </g>
+          )
+        })}
+        {yScaleTicks.map(val => {
+          return (
+            <g>
+              <text
+                textAnchor='middle'
+                dominantBaseline='middle'
+                // @ts-ignore
+                transform={`translate(${xScale("2012") - 20}, ${yScale(val)})`}
+                className='text-[0.6rem] font-bold'
+                fill='black'
+              >
+                {val}
+              </text>
+              <line
+                x1={xScale("2012")}
+                y1={yScale(val)}
+                // @ts-ignore
+                x2={xScale("2021") + xScale.bandwidth()}
+                y2={yScale(val)}
+                stroke='black'
+                strokeOpacity={0.6}
+                strokeDasharray={'3 4'}
+
+              />
+            </g>
+          )
+        })}
+        <line
+          x1={xScale("2012")}
+          y1={yScale(0)}
+          // @ts-ignore
+          x2={xScale("2021") + xScale.bandwidth()}
+          y2={yScale(0)}
+          stroke='black'
+        />
+        <path
+          d={curvePath1}
+          stroke='#2563eb'
+          strokeWidth={1}
+          fill='none'
+        />
+        <path
+          d={curvePath2}
+          stroke='#be123c'
+          strokeWidth={1}
+          fill='none'
+        />
+        {fakeData1.map((data, index) => {
+          return (
+            <g key={`data1-${index}`}>
+              <text
+                textAnchor='middle'
+                dominantBaseline='middle'
+                // @ts-ignore
+                transform={`translate(${xScale(data.year) + xScale.bandwidth()/2}, ${yScale(data.val) - 15})`}
+                className='text-sm font-bold'
+                fill='#2563eb'
+              >
+                {data.val}
+              </text>
+              <circle
+                key={index}
+                r={4}
+                // @ts-ignore
+                cx={xScale(data.year) + xScale.bandwidth()/2}
+                cy={yScale(data.val)}
+                fill='white'
+                stroke='#2563eb'
+                strokeWidth={2}
+              />
+            </g>
+          )
+        })}
+        {fakeData2.map((data, index) => {
+          return (
+            <g key={`data1-${index}`}>
+              <text
+                textAnchor='middle'
+                dominantBaseline='middle'
+                // @ts-ignore
+                transform={`translate(${xScale(data.year) + xScale.bandwidth()/2}, ${yScale(data.val) - 15})`}
+                className='text-sm font-bold'
+                fill='#be123c'
+              >
+                {data.val}
+              </text>
+              <circle
+                key={index}
+                r={4}
+                // @ts-ignore
+                cx={xScale(data.year) + xScale.bandwidth()/2}
+                cy={yScale(data.val)}
+                fill='white'
+                stroke='#be123c'
+                strokeWidth={2}
+              />
+            </g>
+          )
+        })}
+      </svg>
+    </div>
+  )
 }
